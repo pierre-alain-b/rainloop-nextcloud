9712cf953faf304b73ce89a190061f2d51c38463
Tab Fitts <tfitts1212@gmail.com>
2018-09-13 15:04:37 -0700

    Position should be relative, as to prevent overflow

--- rainloop/templates/index.php
+++ rainloop/templates/index.php
@@ -1,1 +1,1 @@
-<iframe id="rliframe" style="border: none; width: 100%; height: 100%; position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px;" tabindex="-1" frameborder="0" src="<?php echo $_['rainloop-iframe-url']; ?>"></iframe><?php OCP\Util::addScript('rainloop', 'resize');
+<iframe id="rliframe" style="border: none; width: 100%; height: 100%; position: relative;" tabindex="-1" frameborder="0" src="<?php echo $_['rainloop-iframe-url']; ?>"></iframe><?php OCP\Util::addScript('rainloop', 'resize');
