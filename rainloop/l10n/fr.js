ef938118a0a62506a15ea525fd96b23e5f6c6a88
Jordan Bieder <jordan.bieder@uliege.be>
2020-02-07 09:15:31 +0100

    Update French translations
    Signed-off-by: Jordan Bieder <jordan.bieder@uliege.be>

--- rainloop/l10n/fr.js
+++ rainloop/l10n/fr.js
@@ -1,11 +1,11 @@
 OC.L10N.register(
     "rainloop",
     {
-    "Email" : " E-mail",
+    "Email" : "Email",
     "RainLoop Webmail" : "RainLoop Webmail",
     "Go to RainLoop Webmail admin panel" : "Se rendre sur la page d'administration de RainLoop Webmail",
-    "Let the user define its own email/password credentials only" : "Uniquement laisser l'utilisateur définir ses propres identifiants email/mot de passe",
-    "Automatically login with Nextcloud user credentials or user defined credentials" : "Automatiquement se connecter avec les identidiants utilisateur de Nextcloud ou les identifiants définis par l'utilisateur",
+    "Let the user define its own email/password credentials only" : "Laisser à l'utilisateur le choix de se connecter automatiquement avec des identifiants qu'il saisira manuellement",
+    "Automatically login with Nextcloud user credentials or user defined credentials" : "Automatiquement se connecter avec les identifiants utilisateur de Nextcloud ou les identifiants définis par l'utilisateur",
     "Automatically login with Nextcloud user email credentials or user defined credentials" : "Automatiquement se connecter avec l'email de l'utilisateur Nextcloud ou les identifiants définis par l'utilisateur",
     "Save" : "Enregistrer"
 },
