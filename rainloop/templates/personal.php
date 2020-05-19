9a6e6a2ed7fae8713f0555e81412687bad2b65ed
Nathan Kinkade <kinkade@measurementlab.net>
2020-05-19 15:04:19 -0600

    Resolves #166 and reverts commit @f5383b8.

--- rainloop/templates/personal.php
+++ rainloop/templates/personal.php
@@ -1,13 +1,8 @@
 <?php
 
 use OCA\RainLoop\Settings\PersonalSettings;
+
 $app = new \OCA\RainLoop\AppInfo\Application();
 $controller = $app->getContainer()->query(PersonalSettings::class);
+return $controller->getForm()->render();
 
-OCP\Util::addScript('rainloop', 'personal');
-
-$oTemplate = new OCP\Template('rainloop', 'personal_settings');
-$oTemplate->assign('rainloop-email', $controller->getEmail());
-$oTemplate->assign('rainloop-password', $controller->getPassword());
-
-return $oTemplate->fetchPage();
