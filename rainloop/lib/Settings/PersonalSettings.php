f5383b80e720397672c3f1d9aa722cbc2a745db6
Pierre-Alain Bandinelli <pa@bandinelli.net>
2020-05-19 08:46:06 +0200

    Fixing #166

--- rainloop/lib/Settings/PersonalSettings.php
+++ rainloop/lib/Settings/PersonalSettings.php
@@ -28,5 +28,15 @@ class PersonalSettings {
 		return new TemplateResponse('rainloop', 'personal_settings', $parameters);
 	}
 
+	public function getEmail() {
+		$uid = \OC::$server->getUserSession()->getUser()->getUID();
+		return $this->config->getUserValue($uid, 'rainloop', 'rainloop-email');
+	}
+
+	public function getPassword() {
+		$uid = \OC::$server->getUserSession()->getUser()->getUID();
+		return $this->config->getUserValue($uid, 'rainloop', 'rainloop-password');
+	}
+
 }
 
