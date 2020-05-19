351784e4f692d355ca3cd034a6cfbf96dd14364b
Valdnet <47037905+Valdnet@users.noreply.github.com>
2020-05-19 19:14:12 +0200

    l10n: Added translation of write messages

--- rainloop/l10n/pl.js
+++ rainloop/l10n/pl.js
@@ -8,6 +8,9 @@ OC.L10N.register(
     "Let the user define its own email/password credentials only" : "Pozwól użytkownikowi zdefiniować tylko własne poświadczenia e-mail/hasło",
     "Automatically login with Nextcloud user credentials or user defined credentials" : "Automatycznie loguj się przy użyciu poświadczeń użytkownika Nextcloud lub poświadczeń zdefiniowanych przez użytkownika",
     "Automatically login with Nextcloud user email credentials or user defined credentials" : "Automatycznie loguj się przy użyciu poświadczeń e-mail użytkownika Nextcloud lub poświadczeń zdefiniowanych przez użytkownika",
-    "Save" : "Zapisz"
+    "Save" : "Zapisz",
+    "Invalid Argument(s)" : "Nieprawidłowe argumenty",
+    "Saved successfully" : "Zapisano pomyślnie",
+    "Error" : "Błąd"
 },
 "nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
