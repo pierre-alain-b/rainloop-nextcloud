643e79e1cfe70bfb0dd24686b5548985b04757fe
Valdnet <47037905+Valdnet@users.noreply.github.com>
2020-05-20 12:46:15 +0200

    l10n: Russian language added

---
+++ rainloop/l10n/ru.js
@@ -0,0 +1,21 @@
+OC.L10N.register(
+    "rainloop",
+    {
+    "RainLoop Webmail" : "РаинЛoоп Вэбмаиль",
+    "Enter an email and password to auto-login to RainLoop." : "Впиши свой адрес электронной почты и пароль для автоматического входа в РаинЛoоп.",
+    "Please note" : "Помни,",
+    "that this feature" : "что эта функция",
+    "may have some" : "может иметь некоторые",
+    "security considerations" : "соображения безопасности",
+    "Email" : "Адрес эл. почты",
+    "Password" : "Пароль",
+    "Go to RainLoop Webmail admin panel" : "Зайдите в админ панель РаинЛoоп Вэбмаиль",
+    "Let the user define its own email/password credentials only" : "Позвольте пользователю определять только свои собственные учетные данные электронной почты/пароля",
+    "Automatically login with Nextcloud user credentials or user defined credentials" : "Автоматический вход в систему с использованием учетные данных пользователя Nextcloud или учетные данные пользователя",
+    "Automatically login with Nextcloud user email credentials or user defined credentials" : "Автоматический вход в систему с использованием учетные данных электронной почты Nextcloud или учетные данные пользователя",
+    "Save" : "Сохранить",
+    "Invalid Argument(s)" : "Неправильные аргументы",
+    "Saved successfully" : "Успешно сохранено",
+    "Error" : "Ошибка"
+},
+"nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
