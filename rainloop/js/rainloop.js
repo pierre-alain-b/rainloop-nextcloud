91e0ca4aafe31699d339df32d0e63d71d5ef4548
Nathan Kinkade <kinkade@measurementlab.net>
2020-05-20 15:43:24 -0600

    Adds unread message count to tab text.

--- rainloop/js/rainloop.js
+++ rainloop/js/rainloop.js
@@ -1,3 +1,39 @@
+// Do the following things once the document is fully loaded.
+document.onreadystatechange = function () {
+	if (document.readyState === 'complete') {
+		watchIFrameTitle();
+	}
+}
+
+// The RainLoop application is already configured to modify the <title> element
+// of its root document with the number of unread messages in the inbox.
+// However, its document is the RainLoop iframe. This function sets up a
+// Mutation Observer to watch the <title> element of the iframe for changes in
+// the unread message count and propagates that to the parent <title> element,
+// allowing the unread message count to be displayed in the NC tab's text when
+// the RainLoop app is selected.
+function watchIFrameTitle() {
+	target = document.getElementById('rliframe').contentDocument.getElementsByTagName('title')[0];
+	config = {
+		characterData: true,
+		childList: true,
+		subtree: true
+	}
+	observer = new MutationObserver(function(mutations) {
+		title = mutations[0].target.innerText;
+		if (title) {
+			matches = title.match(/\(([0-9]+)\)/);
+			if (matches) {
+				unreadCount = matches[1];
+				if (unreadCount) {
+					document.title = t('rainloop', 'Email') + ' (' + unreadCount + ') - Nextcloud';
+				}
+			}
+
+		}
+	});
+	observer.observe(target, config);
+}
 
 function RainLoopFormHelper(sID, sAjaxFile, fCallback)
 {
