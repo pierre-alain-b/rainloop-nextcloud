c0bd327fe8172f4cc34dfcf08eb32ff71a624f5f
Pierre-Alain Bandinelli <pa@bandinelli.net>
2020-05-21 17:17:35 +0200

    Updating FR translation

--- rainloop/l10n/fr.js
+++ rainloop/l10n/fr.js
@@ -1,12 +1,21 @@
 OC.L10N.register(
     "rainloop",
     {
-    "Email" : "Email",
+    "Email" : "Courriel",
     "RainLoop Webmail" : "RainLoop Webmail",
-    "Go to RainLoop Webmail admin panel" : "Se rendre sur la page d'administration de RainLoop Webmail",
+    "Go to RainLoop Webmail admin panel" : "Ouvrir le panneau d'administration de RainLoop Webmail",
     "Let the user define its own email/password credentials only" : "Laisser à l'utilisateur le choix de se connecter automatiquement avec des identifiants qu'il saisira manuellement",
     "Automatically login with Nextcloud user credentials or user defined credentials" : "Automatiquement se connecter avec les identifiants utilisateur de Nextcloud ou les identifiants définis par l'utilisateur",
     "Automatically login with Nextcloud user email credentials or user defined credentials" : "Automatiquement se connecter avec l'email de l'utilisateur Nextcloud ou les identifiants définis par l'utilisateur",
-    "Save" : "Enregistrer"
+    "Save" : "Enregistrer",
+    "Enter an email and password to auto-login to RainLoop." : "Saisissez un identifiant et un mot de passe pour se connecter automatiquement à RainLoop",
+    "Please note" : "Il faut noter",
+    "that this feature" : "que cette fonctionnalité",
+    "may have some" : "peut poser",
+    "security considerations" : "des risques de sécurité",
+    "Password" : "Mot de passe",
+    "Invalid Argument(s)" : "Argument(s) invalide(s)",
+    "Saved successfully" : "Enregistré avec succès",
+    "Error" : "Erreur"
 },
 "nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
