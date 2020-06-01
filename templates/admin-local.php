479b1b1946c813af66888ae54288a98d40fdbde0
Valdnet <47037905+Valdnet@users.noreply.github.com>
2020-06-01 14:07:31 +0200

    l10n: Code unification for text

--- templates/admin-local.php
+++ templates/admin-local.php
@@ -6,12 +6,12 @@
 		<input type="hidden" name="appname" value="rainloop">
 
 		<fieldset class="personalblock">
-			<h2><?php p($l->t('RainLoop Webmail')); ?></h2>
+			<h2><?php echo($l->t('RainLoop Webmail')); ?></h2>
 			<br />
 			<?php if ($_['rainloop-admin-panel-link']): ?>
 			<p>
 				<a href="<?php echo $_['rainloop-admin-panel-link'] ?>" target="_blank" style="text-decoration: underline">
-					<?php p($l->t('Go to RainLoop Webmail admin panel')); ?>
+					<?php echo($l->t('Go to RainLoop Webmail admin panel')); ?>
 				</a>
 			</p>
 			<br />
@@ -20,24 +20,24 @@
 				<div style="display: flex;">
 					<input type="radio" id="rainloop-noautologin" name="rainloop-autologin" value="0" <?php if (!$_['rainloop-autologin']&&!$_['rainloop-autologin-with-email']): ?>checked="checked"<?php endif; ?> />
 					<label style="margin: auto 5px;" for="rainloop-noautologin">
-						<?php p($l->t('Users will login manually, or define credentials in their personal settings for automatic logins.')); ?>
+						<?php echo($l->t('Users will login manually, or define credentials in their personal settings for automatic logins.')); ?>
 					</label>
 				</div>
 				<div style="display: flex;">
 					<input type="radio" id="rainloop-autologin" name="rainloop-autologin" value="1" <?php if ($_['rainloop-autologin']): ?>checked="checked"<?php endif; ?> />
 					<label style="margin: auto 5px;" for="rainloop-autologin">
-						<?php p($l->t('Attempt to automatically login users with their Nextcloud username and password, or user-defined credentials, if set.')); ?>
+						<?php echo($l->t('Attempt to automatically login users with their Nextcloud username and password, or user-defined credentials, if set.')); ?>
 					</label>
 				</div>
 				<div style="display: flex;">
 					<input type="radio" id="rainloop-autologin-with-email" name="rainloop-autologin" value="2" <?php if ($_['rainloop-autologin-with-email']): ?>checked="checked"<?php endif; ?> />
 					<label style="margin: auto 5px;" for="rainloop-autologin-with-email">
-						<?php p($l->t('Attempt to automatically login users with their Nextcloud email and password, or user-defined credentials, if set.')); ?>
+						<?php echo($l->t('Attempt to automatically login users with their Nextcloud email and password, or user-defined credentials, if set.')); ?>
 					</label>
 				</div>
 				<br />
 				<br />
-				<input type="button" id="rainloop-save-button" name="rainloop-save-button" value="<?php p($l->t('Save')); ?>" />
+				<input type="button" id="rainloop-save-button" name="rainloop-save-button" value="<?php echo($l->t('Save')); ?>" />
 				&nbsp;&nbsp;<span class="rainloop-result-desc"></span>
 			</p>
 		</fieldset>
